5f2bc3bc99a7cf22e474e29274f58f863b3e7aee
Saurav Singh <souravsingh.ss508@gmail.com>
2024-08-04 04:00:52 +0530

    add / in href

--- src/Header.js
+++ src/Header.js
@@ -4,7 +4,7 @@ import './Header.css';
 const Header = () => {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark">
-        <a class="navbar-brand" href="#">
+        <a class="navbar-brand" href="/">
             <img src="logo.jpg" width="30" height="30" alt="logo"/>
         </a>
       <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
