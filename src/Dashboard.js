fe55ded3ce8b2be88ba99f97d7ea86904fa951fd
Saurav Singh <souravsingh.ss508@gmail.com>
2024-08-04 13:21:01 +0530

    make responsive for mobile

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -224,7 +224,7 @@ const Dashboard = () => {
                       <thead>
                         <tr>
                           <th>Customer</th>
-                          <th>Order No.</th>
+                          <th className='small-cell'>Order No.</th>
                           <th>Amount</th>
                           <th>Status</th>
                         </tr>
@@ -232,38 +232,38 @@ const Dashboard = () => {
                       <tbody>
                         <tr>
                           <td><img src="/users/2.jpg" class="rounded-circle" alt="Circular" width="25"/>Wade Warren</td>
-                          <td>15478256</td>
+                          <td className='small-cell'>15478256</td>
                           <td>$124.00</td>
                           <td><span className="badge bg-success">Delivered</span></td>
                         </tr>
                         <tr>
                           <td>
                             <img src="/users/1.jpg" class="rounded-circle" alt="Circular" width="25"/>Jane Cooper</td>
-                          <td>48965786</td>
+                          <td className='small-cell'>48965786</td>
                           <td>$385.02</td>
                           <td><span className="badge bg-success">Delivered</span></td>
                         </tr>
                         <tr>
                           <td><img src="/users/7.jpg" class="rounded-circle" alt="Circular" width="25"/>Guy Hawkins</td>
-                          <td>78958215</td>
+                          <td className='small-cell'>78958215</td>
                           <td>$45.88</td>
                           <td><span className="badge bg-danger">Cancelled</span></td>
                         </tr>
                         <tr>
                           <td><img src="/users/3.jpg" class="rounded-circle" alt="Circular" width="25"/>Kristin Watson</td>
-                          <td>20965732</td>
+                          <td className='small-cell'>20965732</td>
                           <td>$65.00</td>
                           <td><span className="badge bg-danger">Pending</span></td>
                         </tr>
                         <tr>
                           <td><img src="/users/4.jpg" class="rounded-circle" alt="Circular" width="25"/>Cody Fisher</td>
-                          <td>95715620</td>
+                          <td className='small-cell'>95715620</td>
                           <td>$545.00</td>
                           <td><span className="badge bg-success">Delivered</span></td>
                         </tr>
                         <tr>
                           <td><img src="/users/5.jpg" class="rounded-circle" alt="Circular" width="25"/>Savannah Nguyen</td>
-                          <td>78514568</td>
+                          <td className='small-cell'>78514568</td>
                           <td>$128.20</td>
                           <td><span className="badge bg-success">Delivered</span></td>
                         </tr>
